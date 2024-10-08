607b91b10d6b345eb750ed0cc3388784bd348f79
ChekinNooget <aldenyuson@gmail.com>
2024-10-07 19:27:40 -0600

    tabs work properly, i could cry

--- index.js
+++ index.js
@@ -13,29 +13,51 @@ setTimeout(() => {
     temp += `<div class="tab_item" onClick="onTabClick('${tempTabName}')">${tempTabName}</div>`;
   }
   mainTabGroup.innerHTML = temp;
-
-  //mainContent.innerHTML = "/main.html"
 }, 50);
-
-/*fetch('page.html')
-  .then(response=> response.text())
-  .then(text=> document.getElementById('elementID').innerHTML = text);
-*/
 
 window.onTabClick = onTabClick;
 function onTabClick(name) {
   selected = name;
-  console.log("get recursive", getRecursive(currentPath.concat(selected)));
-  console.log("get recursive no concat", getRecursive(currentPath));
+  console.log("");
+  //console.log("current path", currentPath);
+  //console.log("selected", selected);
   if (getRecursive(currentPath.concat(selected)) != false) {
     currentPath = currentPath.concat(selected);
-    console.log(currentPath);
   } else if (getRecursive(currentPath.concat(selected)).length == 0) {
-    console.log("empty");
+    currentPath = currentPath.concat(selected);
   } else {
-    currentPath = [];
-    console.log(selected, currentPath);
+    currentPath = [selected];
+    var tempFind = selected
+    while (true) {
+        var tempFound = false
+        for (const a of document.querySelectorAll(".tab_item")) {
+          if (a.textContent == tempFind) {
+            tempFound = true
+            console.log(`.tab_group_${a.parentElement.className.split("tab_group_")[1].split(" ")[0]}`)
+
+            if (a.parentElement.className.split("tab_group_")[1].split(" ")[0] == 0){
+                tempFound = false
+                break
+            }
+
+            tempFind = document.querySelector(`.tab_group_${a.parentElement.className.split("tab_group_")[1].split(" ")[0]-1}`).querySelector(".was_selected").textContent
+            
+            console.log(tempFind)
+            
+            currentPath.unshift(tempFind)
+            break;
+          }
+        }
+        if (!tempFound){
+            console.log("tempfind", tempFind)
+            break
+        }
+    }
+    console.log("currentpath", currentPath)
   }
+  //console.log("get recursive", getRecursive(currentPath.concat(selected)));
+ // console.log("get recursive no concat", getRecursive(currentPath));
+
   //kil lme now
   //ugh
 
@@ -43,24 +65,8 @@ function onTabClick(name) {
   var allTabsWrapper = document.querySelector(".top_tabs");
   var mainContent = document.querySelector(".main_content_wrapper");
   var groupIndex = currentPath.length + 1; //how many rows of things there should be
-
-  mainContent.innerHTML = `<object type="text/html" data="${selected}.html"></object>`;
-
-  //i hate myself for this (temporary)
-
-  //var mainTabGroup = document.querySelector(".tab_group_2"); //needs to not be hardcoded
-
-  //also do append instead of setting it
-
-  //hhhhhhhh
-
-  //console.log(Object.values(data));
   var temp = "";
-  //this barely works. need to make it recursive and check for list
-  /*for (let i = 0; i < Object.keys(data[selected]).length; i++) {
-        var tempTabName = Object.keys(data[selected])[i]
-        temp += `<div class="tab_item" onClick="onTabClick('${tempTabName}')">${tempTabName}</div>`
-    }*/
+
   for (let i = 0; i < getRecursive(currentPath).length; i++) {
     var tempTabName = getRecursive(currentPath)[i];
     temp += `<div class="tab_item" onClick="onTabClick('${tempTabName}')">${tempTabName}</div>`;
@@ -75,7 +81,6 @@ function onTabClick(name) {
   allTabsWrapper.innerHTML = "";
   for (let i = 0; i < currentPath.length + 1; i++) {
     temp = "";
-    //console.log(getRecursive(currentPath.slice(0,i)))
     for (let j = 0; j < getRecursive(currentPath.slice(0, i)).length; j++) {
       var tempTabName = getRecursive(currentPath.slice(0, i))[j];
       temp += `<div class="tab_item" onClick="onTabClick('${tempTabName}')">${tempTabName}</div>`;
@@ -84,6 +89,21 @@ function onTabClick(name) {
     var newTabGroup = `<div class="tab_group_${i} tab_group">${temp}</div>`;
     allTabsWrapper.innerHTML = allTabsWrapper.innerHTML + newTabGroup;
   }
+
+  for (const a of document.querySelectorAll(".tab_item")) {
+    if (a.textContent == selected) {
+      a.classList.add("selected");
+    }
+    if (currentPath.includes(a.textContent)) {
+      a.classList.add("was_selected");
+    }
+  }
+  //console.log(document.querySelector(`.${selected}`).classList)
+  //document.querySelector(`.${selected}`).classList.add("selected")
+
+  //sets the page to the html file
+  console.log(currentPath.slice(0, currentPath.length).join("/"));
+  mainContent.innerHTML = `<object type="text/html" data="/pages/${currentPath.join("/")}.html"></object>`;
 }
 
 function getRecursive(path) {
