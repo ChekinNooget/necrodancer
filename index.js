7761d2bc67153cdaaa1aaae2a7246d1c84726e4a
ChekinNooget <aldenyuson@gmail.com>
2024-10-07 18:07:19 -0600

    doesn't reset when you click on the last tab

--- index.js
+++ index.js
@@ -24,16 +24,17 @@ setTimeout(() => {
 
 window.onTabClick = onTabClick;
 function onTabClick(name) {
-    selected = name;
-  if(getRecursive(currentPath.concat(selected)) != false){
-    currentPath = currentPath.concat(selected)
-    console.log(currentPath)
-  }
-  //else if (getRecursive(currentPath) == [""]){
-  //  console.log(selected, currentPath)}
-   else{
-    currentPath = []
-    console.log(selected, currentPath)
+  selected = name;
+  console.log("get recursive", getRecursive(currentPath.concat(selected)));
+  console.log("get recursive no concat", getRecursive(currentPath));
+  if (getRecursive(currentPath.concat(selected)) != false) {
+    currentPath = currentPath.concat(selected);
+    console.log(currentPath);
+  } else if (getRecursive(currentPath.concat(selected)).length == 0) {
+    console.log("empty");
+  } else {
+    currentPath = [];
+    console.log(selected, currentPath);
   }
   //kil lme now
   //ugh
@@ -41,7 +42,7 @@ function onTabClick(name) {
   //console.log(selected);
   var allTabsWrapper = document.querySelector(".top_tabs");
   var mainContent = document.querySelector(".main_content_wrapper");
-  var groupIndex = currentPath.length + 1//how many rows of things there should be
+  var groupIndex = currentPath.length + 1; //how many rows of things there should be
 
   mainContent.innerHTML = `<object type="text/html" data="${selected}.html"></object>`;
 
@@ -64,25 +65,24 @@ function onTabClick(name) {
     var tempTabName = getRecursive(currentPath)[i];
     temp += `<div class="tab_item" onClick="onTabClick('${tempTabName}')">${tempTabName}</div>`;
   }
-  if(!document.querySelector(`.tab_group_${groupIndex}`)){
-    var newTabGroup = `<div class="tab_group_${groupIndex} tab_group">${temp}</div>`
+  if (!document.querySelector(`.tab_group_${groupIndex}`)) {
+    var newTabGroup = `<div class="tab_group_${groupIndex} tab_group">${temp}</div>`;
   }
-  allTabsWrapper.innerHTML = allTabsWrapper.innerHTML + newTabGroup
+  allTabsWrapper.innerHTML = allTabsWrapper.innerHTML + newTabGroup;
 
-//newf stuff
+  //newf stuff
 
-
-  allTabsWrapper.innerHTML = ""
+  allTabsWrapper.innerHTML = "";
   for (let i = 0; i < currentPath.length + 1; i++) {
-    temp = ""
+    temp = "";
     //console.log(getRecursive(currentPath.slice(0,i)))
-    for (let j = 0; j < getRecursive(currentPath.slice(0,i)).length; j++) {
-      var tempTabName = getRecursive(currentPath.slice(0,i))[j];
+    for (let j = 0; j < getRecursive(currentPath.slice(0, i)).length; j++) {
+      var tempTabName = getRecursive(currentPath.slice(0, i))[j];
       temp += `<div class="tab_item" onClick="onTabClick('${tempTabName}')">${tempTabName}</div>`;
     }
 
-      var newTabGroup = `<div class="tab_group_${i} tab_group">${temp}</div>`
-      allTabsWrapper.innerHTML = allTabsWrapper.innerHTML + newTabGroup
+    var newTabGroup = `<div class="tab_group_${i} tab_group">${temp}</div>`;
+    allTabsWrapper.innerHTML = allTabsWrapper.innerHTML + newTabGroup;
   }
 }
 
@@ -91,15 +91,14 @@ function getRecursive(path) {
   for (let i = 0; i < path.length; i++) {
     tempString = tempString + `['${path[i]}']`;
   }
-    //console.log(eval(`data${tempString}`))
-    //console.log(eval(`Object.keys(data${tempString})`));
+  //console.log(eval(`data${tempString}`))
+  //console.log(eval(`Object.keys(data${tempString})`));
   try {
-    if(eval(`data${tempString}`)){
-        
+    if (eval(`data${tempString}`)) {
     }
-    eval(`Object.keys(data${tempString})`)
+    eval(`Object.keys(data${tempString})`);
     return eval(`Object.keys(data${tempString})`);
-  } catch(err){
-    return false
+  } catch (err) {
+    return false;
   }
 }
